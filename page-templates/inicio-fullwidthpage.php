0d2a2bfeeafc644999dc5249255576baec89df2c
root <root@localhost.localdomain>
2018-03-30 04:04:06 -0300

    Reformando página de inicio. Creación de botones en proceso

--- page-templates/inicio-fullwidthpage.php
+++ page-templates/inicio-fullwidthpage.php
@@ -8,43 +8,57 @@
  */
 
 get_header();
-$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="wrapper" id="full-width-page-wrapper">
+<div class="wrapper" id="inicio-page-wrapper">
 
 	<div class="container-full" id="content">
 
 		<div class="row no-gutters">
-      <a href="" class="col-sm-12 col-md-6" type="button">
-        <img src="http://devhm.com/wp-content/uploads/2018/03/lanzamiento-1250x590.png" alt="">
-      </a>
-      <a href="" class="col-sm-12 col-md-6" type="button">
-        <img src="http://devhm.com/wp-content/uploads/2018/03/leer-1250x590.png" alt="">
-      </a>
+      <div id="buttonCard" class="col-sm-12 col-md-6" >
+        <a href="#" class="text-center">
+          <span>
+           <i class="fab fa-accusoft"></i> Entrar
+          </span>
+          <img src="http://devhm.com/wp-content/uploads/2018/03/lanzamiento-1250x590.png" alt="">
+        </a>
+      </div>
+      <div id="buttonCard" class="col-sm-12 col-md-6" >
+        <a href="#" class="text-center">
+          <span>
+           <i class="fab fa-accusoft"></i> Entrar
+          </span>
+          <img src="http://devhm.com/wp-content/uploads/2018/03/leer-1250x590.png" alt="">
+        </a>
+      </div>
 
       <div class="col-sm-12">
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="http://devhm.com/wp-content/uploads/2018/03/crispin-2500x590.png" alt="First slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="http://devhm.com/wp-content/uploads/2018/03/crispin-2500x590.png" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="http://devhm.com/wp-content/uploads/2018/03/crispin-2500x590.png" alt="Third slide">
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>        
+        <figure class="figure">
+          <figcaption class="display-4 text-center p-3 text-muted">¡Todos los libros!</figcaption>
+          <img src="http://devhm.com/wp-content/uploads/2018/03/crispin-2500x590.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+          <ul class="nav justify-content-end">
+            <li class="nav-item">
+              <a class="nav-link active" href="#"><i class="fab fa-accusoft"></i>
+
+</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fab fa-accusoft"></i>
+
+</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fab fa-accusoft"></i>
+
+</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#"><i class="fab fa-accusoft"></i>
+
+</a>
+            </li>
+          </ul>
+        </figure>
       </div>
 
 
